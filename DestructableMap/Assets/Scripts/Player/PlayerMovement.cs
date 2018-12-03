139c12260904f0569c29b562cab89535f99061f1
blizzard190 <22256@ma-web.nl>
2018-12-03 13:46:10 +0100

    Basic movement

--- DestructableMap/Assets/Scripts/Player/PlayerMovement.cs
+++ DestructableMap/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private int _Speed = 10;
+    private Rigidbody2D _Rb;
+    private Vector2 _MoveVelocity;
+
+    private void Start()
+    {
+        _Rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 MovementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        _MoveVelocity = MovementInput * _Speed;
+    }
+
+    private void FixedUpdate()
+    {
+        _Rb.MovePosition(_Rb.position + _MoveVelocity * Time.deltaTime);
+    }
 }
